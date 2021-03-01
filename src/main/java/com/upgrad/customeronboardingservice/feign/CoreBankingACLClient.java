2c5b6002da4b8689f948ef873724e517f519baf4
abhissaw10 <sawhney.abhishek1983@gmail.com>
2021-03-01 17:33:09 +0530

    Keeping the logic to just synchronous communication between 2 ms

--- src/main/java/com/upgrad/customeronboardingservice/feign/CoreBankingACLClient.java
+++ src/main/java/com/upgrad/customeronboardingservice/feign/CoreBankingACLClient.java
@@ -1,6 +1,5 @@
 package com.upgrad.customeronboardingservice.feign;
 
-import com.upgrad.customeronboardingservice.exception.CoreBankingException;
 import com.upgrad.customeronboardingservice.model.CoreBankingResponseVO;
 import com.upgrad.customeronboardingservice.model.CustomerOnboardRequestVO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,9 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Primary
-@FeignClient(name = "coreBankingAcl", url = "${coreBankingAclURL}",fallback = CoreBankingACLClientFallback.class)
+@FeignClient(name = "coreBankingAcl", url = "${coreBankingAclURL}")
 public interface CoreBankingACLClient {
 
     @PostMapping("/customer/v1/validate")
-    public CoreBankingResponseVO validateCustomerInfo(@RequestBody CustomerOnboardRequestVO customerOnboardRequestVO) throws CoreBankingException;
+    public CoreBankingResponseVO validateCustomerInfo(@RequestBody CustomerOnboardRequestVO customerOnboardRequestVO);
 }
--- src/main/java/com/upgrad/customeronboardingservice/service/CustomerOnboardingService.java
+++ src/main/java/com/upgrad/customeronboardingservice/service/CustomerOnboardingService.java
@@ -11,15 +11,8 @@ import com.upgrad.customeronboardingservice.model.CustomerOnboardResponseVO;
 import com.upgrad.customeronboardingservice.repository.CustomerOnboardingRepository;
 import feign.FeignException;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import static com.upgrad.customeronboardingservice.constants.CustomerOnboardingConstants.ONBOARDING_STATUS;
@@ -30,12 +23,6 @@ public class CustomerOnboardingService {
 
     private CustomerOnboardingRepository repository;
     private CoreBankingACLClient coreBankingACLClient;
-
-    @Value("${notification-topic}")
-    private String notificationTopic;
-
-    @Autowired
-    KafkaTemplate kafkaTemplate;
 
     public CustomerOnboardingService(CustomerOnboardingRepository repository, CoreBankingACLClient coreBankingACLClient) {
         this.repository = repository;
@@ -50,7 +37,7 @@ public class CustomerOnboardingService {
                 .customerMobileNumber(customerOnboardVO.getCustomerMobileNumber())
                 .customerDob(customerOnboardVO.getCustomerDob())
                 .build());
-        notify(customerOnboardVO);
+
         return CustomerOnboardResponseVO
                 .builder()
                 .customerId(customerId)
@@ -76,17 +63,7 @@ public class CustomerOnboardingService {
                 .build();
     }
 
-    private void notify(CustomerOnboardRequestVO customerOnboardRequestVO){
-        kafkaTemplate.send(MessageBuilder
-                .withPayload(customerOnboardRequestVO)
-                .setHeader(KafkaHeaders.TOPIC,notificationTopic)
-                .build());
-    }
 
-    @KafkaListener(topics = "${notification-topic}", groupId = "1")
-    private void listen(CustomerOnboardRequestVO customerOnboardRequestVO){
-        log.info("Received Customer Info "+customerOnboardRequestVO);
-    }
 
     public CustomerOnboardResponseVO getCustomer(String customerId) throws CustomerDoesNotExistException {
         CustomerOboarding customer = repository.findById(customerId)
